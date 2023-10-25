fe347ff3d0fd593d25a00008b4e267fd1844a99a
LeKhanhGithub <khanhcong1909@gmail.com>
2023-10-25 09:17:43 +0700

    add header and footer

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,66 +1,88 @@
-import Image from 'next/image';
-import logo50 from '../../../public/logo50.png';
-import styles from '@/styles/Header.module.css';
-import Link from 'next/link';
+import { Box, Paper, Stack } from "@mui/material";
+import Link from "next/link";
+import logo50 from "../../../public/logo50.png";
+import MenuSection from "./Menu";
+
+const menuData = [
+  {
+    name: "Giới thiệu",
+    subMenus: [
+      { name: "Tầm nhìn và sứ mệnh" },
+      {
+        name: "Bộ máy tổ chức",
+        subMenu2: [
+          "Team Trải nghiệm",
+          "Team Phim nhạc sách",
+          "Team Nhân sự",
+          "Team KTSTN",
+          "Team Truyền thông",
+          "Team Nghiên cứu và phát triển",
+          "Quỹ KTCB",
+        ],
+      },
+      { name: "Thông tin liên hệ" },
+    ],
+  },
+  {
+    name: "Chương trình",
+    subMenus: [
+      { name: "Dự án Cùng bé Trải nghiệm" },
+      {
+        name: "Dự án Phim Nhạc Sách",
+      },
+      { name: "Các chương trình khác" },
+    ],
+  },
+  {
+    name: "Tài chính",
+  },
+  {
+    name: "Bài viết",
+  },
+  {
+    name: "Tham Gia",
+    subMenus: [
+      { name: "Tình nguyện viên" },
+      {
+        name: "Nhà hảo tâm",
+      },
+    ],
+  },
+];
 
 const Header = () => {
   return (
-    <header>
-      <nav id="main-navbar" className="px-3 md:px-5">
-        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-          <Link href="/" className="flex items-center">
-            <Image className={styles.logo} src={logo50.src} height={50} width={50} alt="Logo" />
-          </Link>
-          <div className="flex items-center lg:order-2">
-            <Link href="#" className="btn-filled-pink">Tham gia cùng chúng tôi</Link>
-            <button data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
-              <span className="sr-only">Open main menu</span>
-              <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
-              <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-            </button>
-          </div>
-          <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
-            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-              <li>       
-                <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="font-medium px-4 py-2.5 text-center inline-flex items-center" type="button">Về KTCB
-                </button>
-                <div id="dropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                  <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
-                    <li>
-                      <Link href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Giới thiệu chung về KTCB</Link>
-                    </li>
-                    <li>
-                      <Link href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Giới thiệu về nhân sự KTCB</Link>
-                    </li>
-                  </ul>
-                </div>
-              </li>
-              <li>
-                <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="font-medium px-4 py-2.5 text-center inline-flex items-center" type="button">TNV
-                </button>
-                <div id="dropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                  <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
-                    <li>
-                      <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Tài liệu training</a>
-                    </li>
-                    <li>
-                      <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Nội dung training</a>
-                    </li>
-                    <li>
-                      <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Các hoạt động team building</a>
-                    </li>
-                  </ul>
-                </div>
-              </li>
-              <li>
-                <button className="font-medium px-4 py-2.5 text-center inline-flex items-center" type="button">Hoạt Động - Dự Án
-                </button>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </header>
+    <Stack
+      component={Paper}
+      elevation={4}
+      direction="row"
+      justifyContent="space-between"
+      alignItems="center"
+      px={20}
+      sx={{ py: 2 }}
+    >
+      <Link href="/" className="flex items-center">
+        <Box
+          component="img"
+          width={50}
+          height={50}
+          src={logo50.src}
+          alt="Logo"
+        ></Box>
+      </Link>
+      <Stack
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        spacing={5}
+      >
+        {menuData.map((menu, index) => (
+          <MenuSection key={`${index}menu`} menuData={menu} />
+        ))}
+      </Stack>
+
+      <Box sx={{ width: 50, height: 50, backgroundColor: "red" }}></Box>
+    </Stack>
   );
 };
 
---
+++ src/components/layouts/Menu/index.tsx
@@ -0,0 +1,78 @@
+import { Box, Button, Menu, MenuItem } from "@mui/material";
+import { NestedMenuItem } from "mui-nested-menu";
+import { useState } from "react";
+
+type MenuSectionProps = {
+  menuData: {
+    name: string;
+    subMenus?: { name: string; subMenu2?: string[] }[];
+  };
+};
+
+const MenuSection: React.FC<MenuSectionProps> = ({ menuData }) => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (!!anchorEl) return;
+    setAnchorEl(e.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Box>
+      <Button
+        aria-owns={anchorEl ? "simple-menu" : undefined}
+        aria-haspopup="true"
+        onClick={handleClick}
+        onMouseOver={handleClick}
+        sx={{
+          cursor: "pointer",
+          color: "black",
+          "&:hover": {
+            color: "lightGreen",
+          },
+        }}
+      >
+        {menuData.name}
+      </Button>
+      {!!menuData.subMenus && (
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{ onMouseLeave: handleClose }}
+        >
+          {menuData.subMenus.map((subMenu) => {
+            if (subMenu.subMenu2) {
+              return (
+                <NestedMenuItem
+                  key={subMenu.name}
+                  label={subMenu.name}
+                  parentMenuOpen={open}
+                >
+                  {subMenu.subMenu2.map((name) => (
+                    <MenuItem key={name} data-value={"sub-menu-item"}>
+                      {name}
+                    </MenuItem>
+                  ))}
+                </NestedMenuItem>
+              );
+            }
+            return (
+              <MenuItem key={subMenu.name} onClick={handleClose}>
+                {subMenu.name}
+              </MenuItem>
+            );
+          })}
+        </Menu>
+      )}
+    </Box>
+  );
+};
+
+export default MenuSection;
