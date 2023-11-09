5dda17ea7ba81d0e8e72fd9d05d9ad83146f84d5
MinhHuuNguyen <minhhuunguyen1511@gmail.com>
2023-11-09 14:03:06 +0700

    Add team-cung-be-trai-nghiem-dinh-hinh-tuong-lai-nang-cao-gia-tri

---
+++ src/utils/data/html/team-cung-be-trai-nghiem-dinh-hinh-tuong-lai-nang-cao-gia-tri.ts
@@ -0,0 +1,51 @@
+export default `
+“Cùng bé trải nghiệm” là dự án giáo dục hướng đến các em nhỏ có hoàn cảnh khó khăn tại những trung tâm bảo trợ xã hội trong và ngoài Hà Nội. Với nhiều chủ đề gần gũi, thiết thực nhưng nhưng cũng không kém phần thú vị như “Biết yêu thương bản thân”, “Khéo tay hay làm”, “Ứng phó với tình huống nguy hiểm”,.. dự án tạo cơ hội cho trẻ có thêm những nhận thức, kĩ năng cần thiết, tăng khả năng thích ứng với cuộc sống, môi trường và nghề nghiệp tương lai.
+
+Vị Trí Ứng Tuyển: Giáo Viên Tình Nguyện "Cùng Bé Trải Nghiệm"
+
+Dưới đây là mô tả chi tiết về vị trí dự án cùng bé trải nghiệm:
+1. Mô Tả Công Việc
+
+- Xây dựng phân phối chương trình giảng dạy
+- Soạn giáo án chi tiết cho từng bài theo phân phối chương trình
+- Chuẩn bị đạo cụ, giáo cụ
+- Giảng dạy trực tiếp tại trung tâm
+- Quan sát, đúc kết và rút kinh nghiệm cho bài sau
+
+2. Đối Tượng Tham Gia
+
+- Các bạn trẻ trên địa bàn Hà Nội quan tâm đến hoạt động ngoại khóa, tình nguyện
+- Ưu tiên những bạn yêu thích công việc giảng dạy
+
+3. Quyền Lợi
+
+- Được trực tiếp tham gia vào các hoạt động giảng dạy tại Trung tâm Bảo trợ Xã hội
+- Nhận được giấy chứng nhận từ KTCB sau khi hoàn thành tốt nhiệm vụ.
+- Được tiếp cận với môi trường tình nguyện nghiêm túc và chuyên nghiệp, nơi bạn vừa được làm những việc ý nghĩa, vừa có cơ hội phát triển bản thân.
+- Mở rộng network với nhiều người bạn chung tình yêu với những việc tử tế.
+- Được Co-founders và Mentors hỗ trợ về những mảng kỹ năng quan trọng và cần thiết cho cuộc sống như Leadership, Teamwork, Content, ...
+
+4. Yêu cầu
+
+Về thái độ:
+- Mong muốn được đóng góp cho xã hội, đặc biệt là trong lĩnh vực giáo dục.
+- Tinh thần trách nhiệm cao, tác phong làm việc nghiêm túc, chuyên nghiệp.
+- Chăm chỉ, nhiệt tình, biết lắng nghe đồng thời biết đưa ra ý kiến cá nhân.
+- Có tư duy phản biện.
+- Luôn chủ động trong mọi việc. Đặc biệt chủ động trong việc gắn kết thành viên.
+- Thái độ vui vẻ, hòa đồng, thích học hỏi.
+Về chuyên môn:
+- Kỹ năng quản lý thời gian
+- Có khả năng truyền đạt thông tin tốt
+- Kỹ năng xử lý và giải quyết vấn đề
+
+Trong hành trình "Cùng Bé Trải Nghiệm", chúng ta không chỉ là những người giáo viên, chúng ta là những người truyền đạt niềm tin, là nguồn động viên và là những người hướng dẫn cho những bước đi đầu tiên của những tâm hồn nhỏ bé. Mỗi buổi học là một cơ hội, mỗi em nhỏ là một trang sách mở.
+
+Qua những nụ cười trẻ thơ, qua những giọng nói đầy hứng khởi, chúng ta cảm nhận được ý nghĩa thực sự của việc chia sẻ và giúp đỡ. Đó không chỉ là công việc, mà là trách nhiệm, là tình yêu thương và lòng hiếu khách với cuộc đời. Bằng sự đam mê và nhiệt huyết, chúng ta đang góp phần vào việc xây dựng một xã hội tốt đẹp hơn, nơi mỗi em nhỏ có cơ hội, có niềm tin và có khả năng phát triển.
+
+Khoảng trời của bé chào đón bạn đến với gia đình "Cùng Bé Trải Nghiệm". Hãy cùng nhau tiếp tục hành trình này, để chúng ta không chỉ là những người thầy, mà còn là nguồn động viên không ngừng, phát triển team "Cùng bé trải nghiệm" rộng lớn hơn, đẹp đẽ hơn và ấm áp hơn mỗi ngày. Bởi vì trong trái tim, chúng ta biết rằng mỗi sự chia sẻ và tâm trí mà chúng ta dành cho các em nhỏ không bao giờ là đủ.
+
+Hãy cùng nhau lan tỏa ánh sáng và yêu thương, để ước mơ của mỗi em nhỏ không chỉ là giấc mơ, mà là hiện thực và để tương lai của chúng ta không chỉ là một hy vọng, mà là một hứng khởi đầy ý nghĩa. Cảm ơn bạn đã đồng hành cùng chúng tôi và chia sẻ niềm tin này!
+
+Đăng ký ngay qua link: 
+`;
