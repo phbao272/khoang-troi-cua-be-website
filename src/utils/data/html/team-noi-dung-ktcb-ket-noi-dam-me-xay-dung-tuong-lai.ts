b9d4f476fc2444032b95f45a9b1894df2e87fa14
MinhHuuNguyen <minhhuunguyen1511@gmail.com>
2023-11-09 13:58:27 +0700

    Add team-noi-dung-ktcb-ket-noi-dam-me-xay-dung-tuong-lai

---
+++ src/utils/data/html/team-noi-dung-ktcb-ket-noi-dam-me-xay-dung-tuong-lai.ts
@@ -0,0 +1,54 @@
+export default `
+<span style="font-size: 12pt;">Chào mừng các bạn trẻ yêu thích hoạt động ngoại khóa và tình nguyện tới với Tổ Chức Khoảng Trời Của Bé (KTCB). Chúng tôi hiện đang mở cửa đón nhận ứng tuyển cho vị trí thành viên của team Nội Dung. Đây là cơ hội tuyệt vời để tham gia vào một tổ chức tình nguyện đầy ý nghĩa, nơi bạn có thể thể hiện sự sáng tạo, đam mê và lòng trách nhiệm của mình đối với xã hội và giáo dục. Hãy cùng nhau xây dựng những dự án và chương trình ý nghĩa, mang lại niềm vui và tri thức cho trẻ em Việt Nam!</span>
+
+<span style="font-size: 12pt;">1. Vị trí ứng tuyển</span>
+
+<span style="font-size: 12pt;">Thành viên team Nội dung của tổ chức Khoảng Trời Của Bé, có đam mê với công việc lên kế hoạch tổ chức các dự án và khả năng sáng tạo cao. Team giúp hoạch định, lên ý tưởng và tổ chức các dự án tình nguyện. Trong quá trình hoạt động sẽ được đào tạo phát triển kỹ năng, kiến thức của bản thân và nâng cao chất lượng các thành viên của KTCB.</span>
+
+<span style="font-size: 12pt;">2. Đối tượng tham gia</span>
+
+<span style="font-size: 12pt;">- Các bạn trẻ quan tâm đến hoạt động ngoại khóa, tình nguyện</span>
+<span style="font-size: 12pt;">- Từ 15 – 25 tuổi trên địa bàn Hà Nội</span>
+
+<span style="font-size: 12pt;">3. Mô tả công việc </span>
+
+<span style="font-size: 12pt;">- Tìm kiếm, liên hệ các địa điểm tiềm năng có thể triển khai các dự án tình nguyện:</span>
+<span style="font-size: 12pt;">Tìm kiếm các địa điểm tiềm năng tổ chức dự án/chương trình tình nguyện</span>
+<span style="font-size: 12pt;">Liên hệ các địa điểm để khảo sát đặc điểm và lấy thông tin</span>
+<span style="font-size: 12pt;">- Lập danh sách các ý tưởng về dự án/chương trình muốn triển khai</span>
+<span style="font-size: 12pt;">- Thực hiện xây dựng kế hoạch cho các dự án/chương trình</span>
+<span style="font-size: 12pt;">- Thử nghiệm/ tổ chức thực hiện chương trình thực tế</span>
+<span style="font-size: 12pt;">- Quan sát, đúc kết và rút kinh nghiệm sửa chữa</span>
+
+<span style="font-size: 12pt;">4. Quyền lợi </span>
+
+<span style="font-size: 12pt;">- Nhận được giấy chứng nhận từ KTCB sau khi hoàn thành tốt nhiệm vụ.</span>
+<span style="font-size: 12pt;">- Được tiếp cận với môi trường tình nguyện nghiêm túc và chuyên nghiệp, nơi bạn vừa được làm những việc ý nghĩa, vừa có cơ hội phát triển bản thân.</span>
+<span style="font-size: 12pt;">- Mở rộng network với nhiều người bạn chung tình yêu với những việc tử tế.</span>
+<span style="font-size: 12pt;">- Được Co-founders và Mentors hỗ trợ về những mảng kỹ năng quan trọng và cần thiết cho cuộc sống như Leadership, Teamwork, Content, ...</span>
+
+<span style="font-size: 12pt;">5. Yêu cầu </span>
+
+<span style="font-size: 12pt;">Về thái độ: </span>
+<span style="font-size: 12pt;">- Mong muốn được đóng góp cho xã hội, đặc biệt là trong lĩnh vực giáo dục.</span>
+<span style="font-size: 12pt;">- Tinh thần trách nhiệm cao, tác phong làm việc nghiêm túc, chuyên nghiệp.</span>
+<span style="font-size: 12pt;">- Chăm chỉ, nhiệt tình, biết lắng nghe đồng thời biết đưa ra ý kiến cá nhân.</span>
+<span style="font-size: 12pt;">- Có tư duy phản biện, tinh thần sáng tạo, dám chủ động</span>
+<span style="font-size: 12pt;">- Luôn chủ động trong mọi việc</span>
+<span style="font-size: 12pt;">- Thái độ vui vẻ, hòa đồng, thích học hỏi. </span>
+
+<span style="font-size: 12pt;">Về chuyên môn: </span>
+<span style="font-size: 12pt;">- Có hiểu biết cơ bản về các công việc nghiên cứu và phát triển</span>
+<span style="font-size: 12pt;">- Có kiến thức nền phong phú trên các lĩnh vực liên quan đến từng dự án/chương trình sẽ triển khai</span>
+<span style="font-size: 12pt;">- Có khả năng nhận diện, phân tích đặc điểm phạm vi tổ chức CT/dự án</span>
+<span style="font-size: 12pt;">- Có khả năng lên ý tưởng &amp; Xây dựng kế hoạch cho dự án</span>
+<span style="font-size: 12pt;">- Kỹ năng quản lý thời gian</span>
+<span style="font-size: 12pt;">- Kỹ năng quản lý tài chính</span>
+<span style="font-size: 12pt;">- Có khả năng truyền đạt thông tin tốt </span>
+<span style="font-size: 12pt;">- Kỹ năng xử lý và giải quyết vấn đề </span>
+<span style="font-size: 12pt;">- Sử dụng thành thạo các công cụ Google</span>
+
+<span style="font-size: 12pt;">"Hãy đồng hành cùng chúng tôi tại Khoảng trời của bé, nơi mọi đam mê gặp gỡ và trở thành sức mạnh! Bằng sự chân thành và nhiệt huyết của bạn, chúng ta có thể biến những ý tưởng thành hiện thực, mang niềm vui và tri thức đến cho hàng nghìn trẻ em. Hãy bắt tay vào hành động ngay hôm nay, để chúng ta cùng nhau viết nên những câu chuyện đầy ý nghĩa và hạnh phúc. Hãy làm nên sự khác biệt, bắt đầu từ chúng ta!"</span>
+
+<strong>ĐĂNG KÝ NGAY TẠI:</strong>
+`;
