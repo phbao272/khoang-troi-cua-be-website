5cb29a4e891f4b7718f301b8fc7b4dfffb42c825
phambao <baopq@solashi.com>
2023-11-10 17:55:24 +0700

    use nodemailer

--- src/pages/api/send.ts
+++ src/pages/api/send.ts
@@ -1,21 +1,38 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { resend } from "@/components/emails/resend";
 import WaitlistEmail from "@/components/emails/components/Waitlist";
+
+import { render } from "@react-email/render";
+import nodemailer from "nodemailer";
+import Mail from "nodemailer/lib/mailer";
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "method.apps4@gmail.com",
+    pass: "siqj ikol pool dzyi",
+  },
+});
 
 const send = async (req: NextApiRequest, res: NextApiResponse) => {
   const { method } = req;
 
   switch (method) {
     case "GET": {
-      const data = await resend.emails.send({
-        from: "ktcb@resend.dev",
-        to: ["pqbao27@gmail.com"],
-        subject: "Đăng ký làm nhà hảo tâm",
-        react: WaitlistEmail({ name: "Đăng ký làm nhà hảo tâm" }),
-        text: "Đăng ký làm nhà hảo tâm",
-      });
+      const emailHtml = render(
+        WaitlistEmail({ name: "Đăng ký làm nhà hảo tâm" }) as React.ReactElement
+      );
 
-      return res.status(200).send(data);
+      const options: Mail.Options = {
+        from: "method.apps4@gmail.com",
+        to: "khanhcong1909@gmail.com",
+        subject: "Hello ✔",
+        text: "Hello world?",
+        html: emailHtml,
+      };
+
+      await transporter.sendMail(options);
+
+      return res.status(200).send("send mail successfully!");
     }
     default:
       res.setHeader("Allow", ["GET"]);
