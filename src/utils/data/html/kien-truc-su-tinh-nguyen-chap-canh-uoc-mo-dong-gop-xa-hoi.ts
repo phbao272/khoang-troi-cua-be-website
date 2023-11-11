d3bb2d0edafae8e12c88dd5f386d53a35f6849a8
MinhHuuNguyen <minhhuunguyen1511@gmail.com>
2023-11-11 12:41:28 +0700

    Update all JDs

---
+++ src/utils/data/html/kien-truc-su-tinh-nguyen-chap-canh-uoc-mo-dong-gop-xa-hoi.ts
@@ -0,0 +1,55 @@
+export default `
+<p style="text-align: left;"><span style="font-size: 12pt;"><em><span style="vertical-align: inherit;">Nếu bạn là người yêu thích nhân sự và đam mê phát triển, xây dựng cộng đồng, vị trí team Kiến Trúc Sư Tình Nguyện tại Khoảng Trời Của Bé (KTCB) không chỉ đơn thuần là một công việc mà còn là hoạt động đầy ý nghĩa để xây dựng một xã hội tốt hơn. Tại đây, bạn sẽ không chỉ là thành viên trong team Kiến Trúc Sư Tình Nguyện mà còn là người định hình tương lai cho các thành viên và hướng phát triển của KTCB.</span></em></span></p>
+<p><img class="alignnone size-full wp-image-83" src="http://ktcb.top/wp-content/uploads/2023/11/IMG_2281-scaled.jpg" alt="" width="2560" height="1707" /></p>
+<h4><strong><span style="font-size: 12pt;">VỊ TRÍ ỨNG TUYỂN</span></strong></h4>
+<ul>
+	<li><span style="font-size: 12pt;">Thành viên tổ chức sự kiện</span></li>
+	<li><span style="font-size: 12pt;">Thành viên quản lý và tuyển nhân sự </span></li>
+	<li><span style="font-size: 12pt;">Thành viên định hình chiến lược, văn hóa của tổ chức</span></li>
+</ul>
+<h4 style="text-align: left;"><strong><span style="font-size: 12pt;"><span style="vertical-align: inherit;">ĐỐI TƯỢNG THAM GIA</span></span></strong></h4>
+<p style="text-align: left;"><span style="vertical-align: inherit; font-size: 12pt;">Chúng tôi mở cánh cửa đón những tâm hồn trẻ trung, sáng tạo, trách nhiệm từ Hà Nội và các vùng lân cận. Nếu bạn có niềm đam mê với hoạt động ngoại khóa, tình yêu thực sự với công việc được hỗ trợ cộng đồng và có kinh nghiệm trong lĩnh vực nhân sự thì đây chính là nơi dành cho bạn tại KTCB.</span></p>
+<h4 style="text-align: left;"><strong><span style="font-size: 12pt;"><span style="vertical-align: inherit;">MÔ TẢ CÔNG VIỆC</span></span></strong></h4>
+<ul style="text-align: left;">
+	<li><span style="vertical-align: inherit; font-size: 12pt;">Lên kế hoạch và thực hiện truyền thông nội bộ: Định hình chiến lược truyền thông nội bộ, xây dựng xây dựng văn hóa tổ chức, kích thích tinh thần đồng lòng trong đội ngũ.</span></li>
+	<li><span style="vertical-align: inherit; font-size: 12pt;">Tổ chức sự kiện: Sinh nhật Khoảng Trời Của Bé, sinh nhật theo quý của các thành viên, Tiệc cuối năm, gặp mặt cuối năm...</span></li>
+	<li><span style="vertical-align: inherit; font-size: 12pt;">Tổ chức họp offline tổng kết giữa năm và cuối năm (T6 &amp; T12) và tổng kết team theo từng Quý</span></li>
+	<li><span style="vertical-align: inherit; font-size: 12pt;">Tham gia vào quá trình tuyển dụng, tìm kiếm những tài năng mới cho KTCB</span></li>
+	<li><span style="vertical-align: inherit; font-size: 12pt;">Làm việc chặt chẽ để đảm bảo các dự án và hoạt động của tổ chức được hỗ trợ, tiến hành tốt đẹp.</span></li>
+</ul>
+<h4><img class="alignnone size-full wp-image-84" src="http://ktcb.top/wp-content/uploads/2023/11/16112540_1849176555364457_8774161543186166302_o.jpg" alt="" width="1776" height="999" /></h4>
+<h4 style="text-align: left;"><strong><span style="font-size: 12pt;"><span style="vertical-align: inherit;">YÊU CẦU ỨNG VIÊN </span></span></strong></h4>
+<ul>
+	<li style="text-align: left;"><span style="font-size: 12pt;"><span style="vertical-align: inherit;">Về thái độ:</span></span>
+<ul>
+	<li style="text-align: left;"><span style="vertical-align: inherit; font-size: 12pt;">Mong được đóng góp cho xã hội, đặc biệt là trong lĩnh vực giáo dục.</span></li>
+	<li><span style="vertical-align: inherit; font-size: 12pt;">Tinh thần trách nhiệm cao, tác phong làm việc nghiêm túc, chuyên nghiệp.</span></li>
+	<li><span style="vertical-align: inherit; font-size: 12pt;">Chăm chỉ, nhiệt tình, biết lăng nghe đồng thời biết đưa ra ý kiến ​​cá nhân.</span></li>
+	<li><span style="vertical-align: inherit; font-size: 12pt;">Có tư duy phản biện.</span></li>
+	<li><span style="vertical-align: inherit; font-size: 12pt;">Luôn động động trong mọi công việc. Đặc biệt chủ động trong việc gắn kết thành viên.</span></li>
+	<li><span style="vertical-align: inherit; font-size: 12pt;">Thái độ vui vẻ, hoa đồng, thích học hỏi.</span></li>
+</ul>
+</li>
+	<li style="text-align: left;"><span style="font-size: 12pt;"><span style="vertical-align: inherit;">Về chuyên môn:</span></span>
+<ul>
+	<li><span style="vertical-align: inherit; font-size: 12pt;">Có khả năng gắn kết, cung cấp tinh thần, truyền động lực cho các thành viên</span></li>
+	<li><span style="vertical-align: inherit; font-size: 12pt;">Có kiến ​​thức chuyên môn về nhân sự là điểm cộng</span></li>
+	<li><span style="vertical-align: inherit; font-size: 12pt;">Sử dụng các công cụ Google thành phần</span></li>
+	<li><span style="vertical-align: inherit; font-size: 12pt;">Biết cách xử lý tình huống tốt, linh hoạt.</span></li>
+	<li><span style="vertical-align: inherit; font-size: 12pt;">Khéo léo và chỉn chu trong công việc, biết sắp xếp thời gian hợp lý.</span></li>
+</ul>
+</li>
+</ul>
+<h4 style="text-align: left;"><strong><span style="font-size: 12pt;"><span style="vertical-align: inherit;">QUYỀN LỢI:</span></span></strong></h4>
+<ul style="text-align: left;">
+	<li><span style="vertical-align: inherit; font-size: 12pt;">Nhận được giấy chứng nhận từ KTCB sau khi hoàn thành nhiệm vụ tốt.</span></li>
+	<li><span style="vertical-align: inherit; font-size: 12pt;">Nâng cao khả năng làm việc của nhóm, quản lý nhân sự</span></li>
+	<li><span style="vertical-align: inherit; font-size: 12pt;">Tiếp cận môi trường tình trạng hoàng chất và chuyên nghiệp, nơi bạn vừa được làm những công việc ý nghĩa, vừa có cơ hội phát triển bản thân.</span></li>
+	<li><span style="vertical-align: inherit; font-size: 12pt;">Mạng lưới mở rộng với nhiều người bạn chung tình yêu với những việc tử tế.</span></li>
+	<li><span style="vertical-align: inherit; font-size: 12pt;">Được Co- founders và Mentor hỗ trợ về các mảng kỹ năng quan trọng và cần thiết cho cuộc sống như Lãnh đạo, Làm việc nhóm, Nội dung, …</span></li>
+</ul>
+<p><img class="alignnone size-full wp-image-86" src="http://ktcb.top/wp-content/uploads/2023/11/IMG_7978-scaled.jpg" alt="" width="2560" height="1707" /></p>
+<p style="text-align: left;"><span style="vertical-align: inherit; font-size: 12pt;">Ở Khoảng Trời Của Bé, chúng tôi không chỉ tìm kiếm những người làm việc, chúng tôi tìm kiếm những người truyền cảm hứng. Nếu bạn tự tin thử thách, dám làm và mong muốn đóng góp vào một môi trường tình nguyện, sáng tạo, hãy ứng tuyển ngay hôm nay, hãy trở thành thành viên team Kiến Trúc Sư Tình Nguyện tại Khoảng Trời Của Bé.</span></p>
+<p style="text-align: left;"><span style="font-size: 12pt;"><span style="vertical-align: inherit;">Chúng tôi đang chờ các bạn cùng nhau viết nên những câu chuyện thành công, đầy ý nghĩa và mang lại giá trị cho cộng đồng. Hãy bắt đầu quá trình làm việc, học hỏi của bạn cùng KTCB! </span></span></p>
+<h4><span style="font-size: 12pt;"><strong>ĐĂNG KÝ NGAY <a href="https://docs.google.com/forms/d/e/1FAIpQLSdnW4kvTgjuLViGyr9rhDe6g8cCPtPoJiw4_ob_fuZjNhzo3g/viewform">TẠI ĐÂY</a>!</strong></span></h4>
+`;
