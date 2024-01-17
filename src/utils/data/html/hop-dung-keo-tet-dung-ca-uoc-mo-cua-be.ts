fecaa1d9e5d974fdc4a2789135160330ef80b1e6
MinhHuuNguyen <minhhuunguyen1511@gmail.com>
2024-01-17 15:10:38 +0700

    Add hop-dung-keo-tet-dung-ca-uoc-mo-cua-be

---
+++ src/utils/data/html/hop-dung-keo-tet-dung-ca-uoc-mo-cua-be.ts
@@ -0,0 +1,28 @@
+export default `
+<p style="text-align: justify;"><em><strong>Trong bức tranh đầy màu sắc của cuộc sống, những đứa trẻ với hoàn cảnh khó khăn là những nét bút tình thương cần được vẽ lên và vun đắp mỗi ngày. Chính vì vậy, Khoảng Trời Của Bé tổ chức chương trình bán hàng gây quỹ quà tết cho em, mong muốn mỗi ngày đi qua là một niềm vui khích lệ tinh thần các bé từ những món quà nhỏ. Sản phẩm đầu tiên bày bán chính là hộp đựng kẹo tết - góp chia yêu thương, nơi mỗi chiếc hộp không chỉ là sản phẩm đẹp mắt mà là ánh đèn thắp sáng ước mơ của em. </strong></em></p>
+<p><img class="alignnone size-full wp-image-305" src="http://ktcb.top/wp-content/uploads/2024/01/ee6701af0253a90df042.jpg" alt="" width="1280" height="960" /></p>
+<h3 style="text-align: justify;">Sẻ chia yêu thương mang xuân về gia đình với hộp đựng kẹo tết ý nghĩa </h3>
+<p style="text-align: justify;">Hộp đựng kẹo tết của chúng tôi không chỉ đẹp mắt với thiết kế sang trọng, mà còn là biểu tượng của sự ấm áp và hạnh phúc. Trong mỗi chiếc hộp, chúng tôi gửi gắm tình yêu thương và hy vọng, như những bông hoa xuân khoe sắc giữa không gian gia đình bạn. Sản phẩm được làm bằng gỗ 3mm phủ min kết hợp in màu lên nắp mica, kích thước 16*25*14cm. Với 2 mẫu chủ đề "Chúc Mừng Năm Mới" và "Thần Tài", cùng với 2 tông màu nâu, be, sản phẩm mang đến nhiều sự lựa chọn phong phú để bạn có thể chọn lựa theo sở thích, phong cách của mình.</p>
+<p><img class="alignnone size-full wp-image-299" src="http://ktcb.top/wp-content/uploads/2024/01/416819865_343119945237535_1252053653738319950_n.jpg" alt="" width="1440" height="1080" /></p>
+<p>&nbsp;</p>
+<p style="text-align: justify;">Đặc biệt, khi mua combo 2 hộp đựng kẹo tết đặt cạnh nhau tạo thành ngôi nhà ấm áp - tết sum vầy, phúc lộc thọ. Một bên của ngôi nhà mang đến hình ảnh tết sum vầy, nơi mà gia đình sum họp bình an. Những chiếc hộp đựng kẹo tết kết hợp tạo nên không gian tràn ngập năng lượng tích cực, khơi gợi những kí ức về những khoảnh khắc tròn đầy ý nghĩa và niềm vui trong những ngày đầu năm mới.</p>
+<p style="text-align: justify;">Mặt khác, bên kia của ngôi nhà đặt ra tâm huyết với ý nghĩa của phúc lộc thọ - biểu tượng của sự may mắn, thịnh vượng và sức khỏe. Việc kết hợp hai hộp kẹp tết tạo ra một không gian trang trí độc đáo, nơi mà sự sum vầy của gia đình kết hợp với may mắn tạo nên một bức tranh tinh tế và ý nghĩa.</p>
+<p style="text-align: justify;">Ngôi nhà này là biểu tượng của sự hòa quyện và kết nối giữa hai khái niệm quan trọng trong cuộc sống. Nó là sự gặp gỡ của truyền thống và hiện đại, là sự giao thoa của ý chí, may mắn. Nó truyền tải đầy đủ những giá trị tâm linh và ý nghĩa của Tết đến từng góc nhỏ trong gia đình bạn.</p>
+<p><img class="alignnone size-full wp-image-302" src="http://ktcb.top/wp-content/uploads/2024/01/f342ecb2ef4e44101d5f.jpg" alt="" width="1440" height="1080" /></p>
+<h3 style="text-align: justify;">Dự án bán hàng gây quỹ hộp đựng quà tết - Góp phần xây dựng ước mơ </h3>
+<p style="text-align: justify;"><a href="https://khoangtroicuabe.org/" target="_blank" rel="noopener"><em><strong>Khoảng Trời Của Bé</strong></em></a> tin rằng từ những điều nhỏ bé, chúng ta có thể xây dựng những ước mơ lớn lao. Với mỗi sản phẩm hộp đựng quà tết bán ra, chúng ta đang góp phần vào hành trình gây quỹ, giúp những tia hy vọng len lỏi trong những đứa trẻ đang gặp khó khăn.</p>
+<p style="text-align: justify;">Mỗi phần lợi nhuận từ sản phẩm sẽ được dành để tặng những phần quà ý nghĩa hỗ trợ trẻ em tại Trung Tâm Hy Vọng và hỗ trợ 2 dự án lớn của KTCB chính là Yêu Cùng Bé (chi phí mua sách, mua hoặc thuê nhạc cụ, mua đồ ăn vặt trong những buổi chiếu phim) và Cùng Bé Trải Nghiệm (chi phí chuẩn bị giáo cụ cho mỗi bài giảng, phần thưởng trò chơi, hoạt động trải nghiệm...). </p>
+<p><img class="alignnone size-full wp-image-298" src="http://ktcb.top/wp-content/uploads/2024/01/415275776_1041729080233097_5119526142981508432_n.jpg" alt="" width="1440" height="1080" /></p>
+<h3 style="text-align: justify;">Hộp đựng kẹo tết - Lan tỏa yêu thương, một mùa tết ấm cúng </h3>
+<p style="text-align: justify;">"Chung tay yêu thương" không chỉ là một khẩu hiệu, mà là tâm huyết Khoảng Trời Của Bé muốn chia sẻ. Mỗi sản phẩm hộp đựng kẹo tết bán ra là một bước chân vào thế giới của những trái tim nhân ái, nơi tình yêu thương lan tỏa như ánh sáng mặt trời sau cơn mưa. Hãy cùng chúng tôi tạo ra một Xuân Tết đặc biệt, nơi mỗi đứa trẻ có hoàn cảnh khó khăn đều được cảm nhận tình yêu và sự quan tâm.</p>
+<p style="text-align: justify;">Hãy tham gia cùng Khoảng Trời Của Bé, chọn "Hộp đựng kẹo tết" để mở lời tình cảm, mang lại niềm vui cho những nụ cười và bước chân nhỏ bé trên con đường đầy thách thức của những thiên thần nhỏ!</p>
+<blockquote>
+<p style="text-align: justify;"><strong>Liên hệ đặt hàng ngay:</strong></p>
+</blockquote>
+<p style="text-align: justify;">Số điện thoại: <strong>0869847693</strong></p>
+<p>Fanpage: <a href="https://www.facebook.com/khoangtroicuabektcb?locale=vi_VN" target="_blank" rel="noopener">https://www.facebook.com/khoangtroicuabektcb</a></p>
+<p style="text-align: justify;">Email: khoangtroicuabe.kby@gmail.com </p>
+<blockquote>
+<p style="text-align: justify;"><strong>Khoảng Trời Của Bé chân thành cảm ơn sự ủng hộ của mọi người! </strong></p>
+</blockquote>
+`;
