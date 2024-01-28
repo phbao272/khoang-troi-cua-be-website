51d590714f2e26b029bbfb525f5e28310b80dddb
Doan The Vu <vu.doanthe@pixta.co.jp>
2024-01-28 11:08:58 +0700

    Fix build

---
+++ src/db/seed.ts
@@ -0,0 +1,33 @@
+import { PrismaClient, Prisma } from '@prisma/client'
+import bcrypt from 'bcryptjs'
+
+const prisma = new PrismaClient()
+
+const userData = [
+  {
+    username: 'KTCB_Admin',
+    email: 'ktcb@gmail.com',
+    password: await bcrypt.hash('ktcbtest', 10)
+  }
+]
+
+async function main() {
+  console.log(`Start seeding ...`)
+  for (const u of userData) {
+    const user = await prisma.user.create({
+      data: u,
+    })
+    console.log(`Created user with id: ${user.id}`)
+  }
+  console.log(`Seeding finished.`)
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
