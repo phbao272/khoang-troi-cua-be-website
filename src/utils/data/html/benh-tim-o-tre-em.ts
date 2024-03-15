55b93949e790e94d07e9c29d45bef1ac07747630
MinhHuuNguyen <minhhuunguyen1511@gmail.com>
2024-03-15 10:23:05 +0700

    Add benh-tim-o-tre-em

---
+++ src/utils/data/html/benh-tim-o-tre-em.ts
@@ -0,0 +1,42 @@
+export default `
+<p><em><strong>Ở đâu đó ngoài kia vẫn còn rất nhiều mảnh đời, nhiều trái tim lỗi nhịp của các em nhỏ đang từng ngày cạn kiệt hy vọng, điều mà các em mong muốn nhất chắc chắn là sự bình an với một trái tim mạnh khoẻ. Khoảng Trời Của Bé tin rằng với sức lan tỏa cùng sự đóng góp của các nhà hảo tâm sẽ đem đến nguồn động lực cho các “ chiến binh nhí”.</strong></em></p>
+<h3>Bệnh tim ở trẻ em được hiểu như thế nào ?</h3>
+<p>Bệnh tim ở trẻ em hay còn được gọi là bệnh tim bẩm sinh (CHD) là khi còn trong bào thai thì cơ tim, buồng tim, van tim xuất hiện những dị tật cho đến khi đã sinh ra. Chức năng và hoạt động của tim đã bị ảnh hưởng do cấu trúc tim bị khiếm khuyết dẫn đến việc các tuần hoàn máu trong cơ thể cũng hoạt động bất thường.</p>
+<p>Bệnh tim ở trẻ em là một trong những ca dị tật phổ biến và nguyên nhân gây tử vong lớn hàng đầu cũng là từ căn bệnh tim bẩm sinh. Tại các nước khác trên thế giới, tỷ lệ các ca bệnh tim ở trẻ em nằm trong khoảng từ 0,7 - 1% còn tại Việt Nam thì tỷ lệ mắc bệnh tim bẩm sinh ở trẻ em là vào khoảng 1,5% theo báo cáo của các bệnh viện nhi.</p>
+<p><img class="aligncenter" src="https://vinmec-prod.s3.amazonaws.com/images/20191016_112941_140338_nguyen-nhan-suy-tim-c.max-800x800.jpg" /></p>
+<h3>Vậy biểu hiện của bệnh tim ở trẻ em sẽ xuất hiện những triệu chứng gì?</h3>
+<p>Các dấu hiệu bệnh tim ở trẻ em được phát hiện và chẩn đoán kịp thời khi trẻ mặc những triệu chứng khác lạ dưới đây:</p>
+<ul>
+	<li>Trẻ không khóc khi vừa được sinh ra</li>
+	<li>Da xanh xao, hay vã mồ hôi</li>
+	<li>Thở nhanh, khó thở, lõm ngực</li>
+	<li>Bị viêm phổi hoặc nhiễm trùng hô hấp</li>
+	<li>Chậm phát triển về thể chất, tâm thần</li>
+	<li>Tim to, tim đập bất thường</li>
+	<li>Suy tuần hoàn được xác định là biểu hiện đầu tiên của những ca bệnh tim bẩm sinh ở trẻ em. Trẻ sơ sinh sẽ có những biểu hiện mệt nhiều, mạch yếu, huyết áp thấp,...</li>
+</ul>
+<p><img class="aligncenter" src="https://www.vinmec.com/s3-images/20231130_024413_683445_suy_tim.max-800x800.jpg" /></p>
+<h3>Bệnh tim ở trẻ em được điều trị bằng phương pháp nào ?</h3>
+<p>Việc phát hiện cũng như cho ra những phác đồ điều trị còn phải phụ thuộc vào mức độ nghiêm trọng của dị tật. Nhẹ thì sẽ tự lành theo thời gian còn những trường hợp không may mắn thì cần quá trình điều trị lâu dài theo một số phương pháp sau:</p>
+<ul>
+	<li>Thuốc</li>
+	<li>Thiết bị cấy ghép tim</li>
+	<li>Thủ tục ống thông tim</li>
+	<li>Phẫu thuật mổ mở</li>
+	<li>Ghép tim</li>
+</ul>
+<p>Việc điều trị bệnh tim bẩm sinh ở người lớn, trẻ nhỏ là khác nhau, tùy thuộc vào mức độ nghiêm trọng của bệnh đang ở đâu. Một số ca thì chỉ cần theo dõi tình trạng và những ca nặng hơn thì có thể cần đến thuốc, phẫu thuật.</p>
+<p><img class="aligncenter" src="https://isofhcare-backup.s3-ap-southeast-1.amazonaws.com/images/co-che-ton-thuong-va-trieu-chung-benh-co-tim-han-che-ivie-(5)_d47d8f4c_fa5c_4fb4_9a66_4bfbf354916f.jpg" /></p>
+<h3>Những trái tim cần được chữa lành</h3>
+<p>Ở đâu đó ngoài kia còn rất nhiều những hoàn cảnh khó khăn, những mảnh đời bất hạnh đang từng ngày cạn kiệt hy vọng. Thấu hiểu được sự đau đớn và việc phải sống khổ sở từng ngày vì căn bệnh tim bẩm sinh dày vò thì một cuộc sống khỏe mạnh, bình an với một trái tim mới khoẻ mạnh hơn có lẽ là điều mà các em nhỏ đang hàng ngày mong ước.</p>
+<p>May mắn không mỉm cười khi sinh ra đã mang trong mình căn bệnh tim bẩm sinh, gia đình các em nhỏ lại thuộc diện có hoàn cảnh khó khăn, bố mẹ là lao động nghèo không có đủ khả năng để chi trả tiền thuốc men cũng như số tiền phẫu thuật lên đến hàng trăm triệu đồng. Bố mẹ chỉ có thể bất lực nhìn con cái chịu đau đớn từng ngày vì gia đình không thể xoay sở được số tiền lớn như vậy.</p>
+<p><a href="https://vtv.vn/tam-long-viet/trai-tim-khong-lanh-lan-cua-cau-be-8-tuoi-dang-can-duoc-chua-lanh-20220429161425056.htm?fbclid=IwAR2X0jS770hNRJ1HGsi9kxX2BW_-pUbAl9qKQF5sSmmj1ImAI1r7SeSDADI" target="_blank" rel="noopener">Trái tim không lành của cậu bé 8 tuổi</a>.</p>
+<p><a href="https://vtv.vn/tam-long-viet/hoan-canh-kho-khan-cua-be-trai-10-thang-tuoi-mac-benh-tim-bam-sinh-2022042712125806.htm?fbclid=IwAR0W5UdeFr0KbI4-YIHg5ktmcxBKy8ltYARhYAPrEcqs7nQglkhFWldJa0A" target="_blank" rel="noopener">Hoàn cảnh khó khăn của bé trai 10 tuổi</a></p>
+<p><a href="https://vtv.vn/tam-long-viet/be-trai-1-tuoi-can-so-tien-lon-de-phau-thuat-tim-bam-sinh-lan-2-20220425144211378.htm?fbclid=IwAR1nieOTCDqLqjF1_mh07B7i8mrkvUXqu-s156Bke64722F4hlF05ozjvHE" target="_blank" rel="noopener">Bé trai 1 tuổi cần số tiền để phẫu thuật</a></p>
+<p>....</p>
+<p><img class="aligncenter" src="https://vnvc.vn/wp-content/uploads/2020/12/phong-ngua-benh-tim.jpg" alt="" width="800" height="571" /></p>
+<h3>Nguyện vọng của Khoảng Trời Của Bé</h3>
+<p>Thấu hiểu được những khó khăn, thương xót cho các em nhỏ đang phải hứng chịu sự dày vò của căn bệnh tim bẩm sinh, Khoảng Trời Của Bé đã và đang đồng hành cùng các em nhỏ trên con đường chiến đấu với bệnh tật, mang phép màu đến với mỗi gia đình của các bé.</p>
+<p>Khoảng Trời Của Bé tin rằng với sức lan tỏa, giúp đỡ, cùng sự đóng góp của các nhà hảo tâm sẽ đem lại nguồn động lực cũng như tiếp thêm sức mạnh cho mỗi em nhỏ. Mỗi trường hợp đến với Khoảng Trời Của Bé đều có hoàn cảnh rất đáng thương, cần sự sẻ chia và các mạnh thường quân. Mỗi sự đóng góp dù là ít hay nhiều cũng góp phần tạo nên kỳ tích và tiếp thêm rất nhiều hy vọng cho các gia đình để có thêm chi phí phẫu thuật giúp các bé sớm được khoẻ mạnh, trở về với trái tim bình an.</p>
+<p>Khoảng Trời Của Bé sẽ luôn đồng hành cùng các em nhỏ trên hành trình chiến đấu với căn bệnh, mong rằng sẽ có thật nhiều những nhà hảo tâm tới ủng hộ các em để các em thêm nhiều niềm vui, sức khoẻ. Một lần nữa Khoảng Trời Của Bé xin chúc các em sẽ chiến thắng bệnh tật, có một trái tim khỏe mạnh và đón nhận một cuộc sống tốt đẹp hơn.</p>
+`;
