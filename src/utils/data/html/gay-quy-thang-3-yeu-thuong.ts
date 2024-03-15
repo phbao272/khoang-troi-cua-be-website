0e067be17e49a6113ae36bfc6114c39a2e69a4e8
MinhHuuNguyen <minhhuunguyen1511@gmail.com>
2024-03-15 09:53:14 +0700

    Add gay-quy-thang-3-yeu-thuong

---
+++ src/utils/data/html/gay-quy-thang-3-yeu-thuong.ts
@@ -0,0 +1,31 @@
+export default `
+<p><em><strong>Mỗi năm, tháng 3 đưa đến không chỉ là sự khởi đầu của mùa xuân, mà còn là thời điểm của những nỗ lực tuyệt vời nhằm hỗ trợ cộng đồng và tạo ra những thay đổi tích cực trong xã hội. Trong không khí của sự tươi mới và hy vọng, Khoảng Trời Của Bé đã thực hiện chương trình gây quỹ "Tháng 3 Yêu Thương" với mục đích gây quỹ để hỗ trợ bữa ăn cho các em nhỏ tại Trung tâm bảo trợ xã hội số III - cơ sở 3, toàn bộ lợi nhuận từ chương trình sẽ được sử dụng để hỗ trợ sữa cho các em nhỏ.</strong></em></p>
+<p><img class="size-full wp-image-382 aligncenter" src="http://ktcb.top/wp-content/uploads/2024/03/3-1.png" alt="" width="804" height="449" /></p>
+<h3>Bạn đồng hành cùng Khoảng trời của bé<b></b></h3>
+<p><span style="font-weight: 400;">Với dự án “Tháng 3 yêu thương” lần này, Khoảng Trời Của Bé đã hợp tác với bạn Thảo Chanh. Bắt đầu từ một tình yêu sâu đậm với bánh ngọt, đối với Thảo việc tạo ra từng chiếc bánh không chỉ là một công việc, mà còn là hành trình tạo ra những sự ngọt ngào thỏa mãn nhu cầu thực khách. Thảo tập tành học làm bánh từ khi đang còn trên ghế trung học phổ thông, niềm đam mê ấy vẫn tiếp tục khi Thảo vào đại học, cô gái ấy đã không ngừng học hỏi và mong ước sau này có thể mở được tiệm bánh của riêng bản thân mình. </span></p>
+<p><span style="font-weight: 400;">Thảo chia sẻ rằng nguồn cảm hứng của mình đến từ những hương vị và màu sắc của cuộc sống. Những kỷ niệm từ tuổi thơ, hương vị của bánh mứt mà bà nội từng làm, hay bánh sinh nhật được làm tại gia đình đã làm cho Thảo tìm kiếm niềm vui trong việc tạo ra những loại bánh ngon miệng cho người khác.</span></p>
+<p><span style="font-weight: 400;">Là một thợ làm bánh có trách nhiệm, Thảo yêu cầu cao trong tất cả các khâu để tạo ra những chiếc bánh thơm ngon, từ khâu chọn nguyên liệu chất lượng đến điều chỉnh công thức tỉ mỉ, sản phẩm làm ra đều đạt chất lượng tốt và được nhiều khách hàng đánh giá cao, cứ mỗi dịp lễ hay tết thì sản phẩm nhà Thảo đều cháy hàng, bánh ngon mà giá cả lại phù hợp nên mọi người rất săn đón. Khoảng Trời Của Bé cảm thấy an tâm khi hợp tác với Thảo để mang đến cho mọi người những sản phẩm chất lượng và thơm ngon.</span></p>
+<p><span style="font-weight: 400;">Với lần hợp tác tháng 3 yêu thương này này, Thảo rất mong có thể bán được nhiều sản phẩm để có thể giúp đỡ được nhiều em nhỏ có hoàn cảnh khó khăn.</span></p>
+<p><img class="size-full wp-image-383 aligncenter" src="http://ktcb.top/wp-content/uploads/2024/03/2-1.png" alt="" width="1160" height="820" /></p>
+<h3>Chi tiết chương trình gây quỹ tháng 3 yêu thương</h3>
+<p><span style="font-weight: 400;">Chương trình diễn ra từ ngày 01/03/2022 đến hết ngày 06/03/2022, mọi người đặt bánh sẽ được miễn phí vận chuyển kèm theo một chiếc thiệp nhỏ xinh từ chương trình để có thể gửi những lời chúc tốt đẹp cũng như những lời yêu thương đến những người mà bạn yêu quý. Sản phẩm cũng rất thích hợp để làm quà tặng nhân dịp 8/3 hay sinh nhật, lễ kỉ niệm, vừa làm vui lòng những người thân, bạn bè vừa góp phần giúp đỡ các em nhỏ có thêm những bữa ăn đầy đủ hơn.</span></p>
+<p><span style="font-weight: 400;">Sản phẩm chương trình bán gồm có:</span></p>
+<p><span style="font-weight: 400;"> B01: Bánh quy icing bán lẻ</span></p>
+<p><span style="font-weight: 400;"> B02: Bánh quy icing hộp 5 chiếc</span></p>
+<p><span style="font-weight: 400;"> B03: Bánh quy icing hộp 8 chiếc</span></p>
+<p><span style="font-weight: 400;"> B04: Bánh dứa Đài Loan</span></p>
+<p><span style="font-weight: 400;"> B05: Set bánh quy</span></p>
+<p><i><span style="font-weight: 400;">Với bánh quy icing:</span></i><span style="font-weight: 400;"> Bánh được tạo hình tinh tế, được vẽ thủ công 100% , thành phần ngoài các nguyên liệu cơ bản, còn có đường mật mía Molasses, mật ong làm bánh mềm xốp và thơm ngon hơn, màu thực phẩm Wilton của Mỹ nên khách hàng có thể yên tâm sử dụng.</span></p>
+<p><i><span style="font-weight: 400;">Bánh dứa Đài Loan:</span></i><span style="font-weight: 400;"> Vỏ bánh giòn thơm mùi sữa với nhân mứt dứa thơm lừng,dẻo dẻo chua ngọt vừa đủ. Chiếc bánh bé xinh này chắc sẽ được các bà, các mẹ ưa thích khi uống trà.</span></p>
+<p><i><span style="font-weight: 400;">Set bánh quy</span></i><span style="font-weight: 400;">: Set này gồm một hộp bánh dứa và một hộp bánh quy socola chip hạnh nhân dùng làm quà tặng khi đến nhà người thân hoặc người yêu rất phù hợp.</span></p>
+<p><img class="size-full wp-image-380 aligncenter" src="http://ktcb.top/wp-content/uploads/2024/03/5.png" alt="" width="1200" height="800" /></p>
+<h3>Tổng kết chương trình gây quỹ tháng 3 yêu thương<b></b></h3>
+<p><span style="font-weight: 400;">Trong dự án tháng 3 yêu thương lần này, với mỗi chiếc bánh nhỏ được mọi người mua thì chính các bạn cũng đã góp một phần công sức trong việc hỗ trợ cho các em nhỏ có hoàn cảnh khó khăn có thêm được những bữa ăn ngon. Tổng kết chương trình, Khoảng Trời Của Bé đã bán được:</span></p>
+<p><span style="font-weight: 400;"> 25 bánh dứa Đài Loan.</span></p>
+<p><span style="font-weight: 400;"> 21 bánh icing hộp 5 chiếc.</span></p>
+<p><span style="font-weight: 400;"> 4 set bánh quy.</span></p>
+<p><span style="font-weight: 400;">Tổng lợi nhuận Khoảng Trời Của Bé thu được từ chương trình lần này là 1.261.000 VNĐ (Một triệu hai trăm sáu mươi mốt nghìn đồng) và toàn bộ số tiền này sẽ được chuyển vào quỹ KTCB để hỗ trợ bữa ăn cho các em nhỏ tại Trung tâm bảo trợ xã hội số III - cơ sở 3.</span></p>
+<p><span style="font-weight: 400;">Phản hồi của khách hàng đối với sản phẩm rất tích cực về cả chất lượng lẫn hình thức, bánh ngon và bao bì gói quà thì bắt mắt, mọi người đều hài lòng về hoạt động được tổ chức lần này, ý nghĩa và thiết thực.</span></p>
+<p><img class="wp-image-381 size-full aligncenter" src="http://ktcb.top/wp-content/uploads/2024/03/4-1.png" alt="" width="364" height="363" /></p>
+<p><span style="font-weight: 400;">Khoảng Trời Của Bé xin chân thành cảm ơn tất cả mọi người đã góp phần tạo nên thành công cho dự án tháng 3 yêu thương lần này, đặc biệt cảm ơn đến đối tác của chúng ta - bạn Thảo Chanh. Mong rằng trong khoảng thời gian sắp tới sẽ tiếp tục nhận được sự quan tâm và ủng hộ từ các bạn. Thêm vào đó chúng mình cũng hy vọng rằng sẽ có thêm thật nhiều dự án thiện nguyện để mọi người có thể cùng chung tay chia sẻ yêu thương đến những hoàn cảnh kém may mắn hơn trong cuộc sống.</span></p>
+`;
